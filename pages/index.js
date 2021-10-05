53ca09c6d128ff833691c428f9044fdc4d34536b
lturpin <laurencejosephturpin@gmail.com>
2021-10-05 19:52:30 +0100

    added netlifyAuth.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useEffect, useState } from 'react';
+import netlifyAuth from '../netlifyAuth.js';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+  let [user, setUser] = useState(null);
+
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+      netlifyAuth.closeModal();
+    });
+  };
+
+  let logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(false);
+      setUser(null);
+    });
+  };
+
+  useEffect(() => {
+    netlifyAuth.initialize((user) => {
+      setLoggedIn(!!user);
+    });
+  }, [loggedIn]);
   return (
     <div className="container">
       <Head>
@@ -12,6 +37,11 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
+        {loggedIn ? (
+          <div>You are logged in!</div>
+        ) : (
+          <button onClick={login}>Log in here.</button>
+        )}
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
@@ -19,5 +49,5 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
 }
